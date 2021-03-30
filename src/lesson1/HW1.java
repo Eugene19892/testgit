5861f5f66347c8480b425ecb6b321fb0e37b448d
Eugene19892 <jackevgen1989@gmail.com>
2021-03-30 16:56:47 +0300

    Прошла лекци №1

--- src/lesson1/HW1.java
+++ src/lesson1/HW1.java
@@ -3,58 +3,59 @@ package lesson1;
 public class HW1 {
 
     //метод 1
-    public static void main(String[] args){
-
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-        float result;
-
-        result = a * (b + (c / d));
-
-        result = myMath(a, b, c, d);
-
-        System.out.println(result);
-
-
-    }
-
-    public static float myMath(int valueA, int valueB, int valueC, int valueD) {
-        return valueA * (valueB + ((float)valueC / valueD));
-
-
-    }
-}
-
-
-// метод 2
-//     static boolean myMath (int a, int b){
+    //   public static void main(String[] args){
 //
-//            int sum = a + b;
-//           if (sum > 10 && sum < 20) return true;
-//           else return false;
-//       }
-//       public static void main(String[] args){
-//        System.out.println( myMath(10, 90) );
+//        int a = 1;
+//        int b = 2;
+//        int c = 3;
+//        int d = 4;
+//        float result;
+//
+//        result = a * (b + (c / d));
+//
+//        result = calculate(a, b, c, d);
+//
+//        System.out.println(result);
+//
+//
 //    }
-
-
-//метод 3
-//    public static void main(String[] args) {
-
-//        int a = 3, b = 3, c = 0;
 //
-//        c = a - b; // с = 3 - 3 = 0
-//        System.out.println("с = " + c);
-//        if (c >= 0) {
-//            System.out.println("с не отрицательно");
-//        }
-//        if (c < 0) {
+//    public static float calculate(int valueA, int valueB, int valueC, int valueD) {
+//        return valueA * (valueB + ((float)valueC / valueD));
 //
-//        }
+//
 //    }
 //}
+
+
+//    метод 2
+//    static boolean myMath(int a, int b) {
+//
+//        int sum = a + b;
+//        if (sum >= 10 && sum <= 20) return true;
+//        else return false;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(myMath(1, 15));
+//    }
+//}
+
+//метод 3
+    public static void main(String[] args) {
+
+        int a = 3, b = 3, c = 0;
+
+        c = a - b; // с = 3 - 3 = 0
+        System.out.println("с = " + c);
+        if (c >= 0) {
+            System.out.println("с не отрицательно");
+        }
+        if (c < 0) {
+
+        }
+    }
+}
 
 
 //метод 4
@@ -71,3 +72,5 @@ public class HW1 {
 
 
 //}
+
+
